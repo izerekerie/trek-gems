fc214dbec420761de01693dc18ee2cb421c9b385
izerekerie <izereuwonkundakelia@gmail.com>
2025-04-02 17:26:43 +0200

    fix: backend url

--- services/api.ts
+++ services/api.ts
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const API_URL = process.env.BACKEND_URL;
-
-// Create the axios instance
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 const apiClient = axios.create({
   baseURL: API_URL,
