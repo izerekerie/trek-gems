2e099355daf2e6c9e96b66b864edee3f04c96da2
izerekerie <izereuwonkundakelia@gmail.com>
2025-04-03 01:37:43 +0200

    remove local api

--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-//const API_URL = process.env.NEXT_PUBLIC_API_URL;
-const API_URL = "http://localhost:3000";
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+//const API_URL = "http://localhost:3000";
 
 const apiClient = axios.create({
   baseURL: API_URL,
